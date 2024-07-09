23418e3102432c7e8bc9f5ac0d79238cc7d2fd8e
BsoBird <plashspeed@foxmail.com>
2024-07-09 15:57:31 +0800

    add lockManager

---
+++ core/src/main/java/org/apache/iceberg/lock/RedissonLockManager.java
@@ -0,0 +1,149 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  *   http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package org.apache.iceberg.lock;
+
+import java.util.Base64;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import org.apache.iceberg.exceptions.BadRequestException;
+import org.apache.iceberg.relocated.com.google.common.base.Strings;
+import org.apache.iceberg.relocated.com.google.common.collect.Maps;
+import org.apache.iceberg.util.LockManagers;
+
+import org.apache.iceberg.util.Tasks;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RedissonLockManager extends LockManagers.BaseLockManager{
+
+    public final static String REDISSON_LOCK_CONF = "lock.redisson.conf.yml.base64";
+    public final static String REDISSON_LOCK_URL = "lock.redisson.conf.url";
+    public final static String REDISSON_LOCK_KEY_PREFIX = "lock.redisson.key.prefix";
+    public final static String REDISSON_LOCK_RETRY_MAX_TIMES = "lock.redisson.retry.max-times";
+    private static final Logger LOG = LoggerFactory.getLogger(RedissonLockManager.class);
+
+    private final Map<String,RLock> lockCache = Maps.newHashMap();
+    private RedissonClient redissonClient;
+    private String prefix = null;
+    private int maxRetryTimes = 10;
+    public RedissonLockManager(){}
+
+    public RedissonLockManager(String redisUrl){
+        Config config = new Config();
+        config.useClusterServers().addNodeAddress(redisUrl);
+        redissonClient = Redisson.create(config);
+    }
+
+    @Override
+    public void initialize(Map<String, String> properties) {
+        try{
+            super.initialize(properties);
+            String redissonConfYmlBase64 = properties.getOrDefault(REDISSON_LOCK_CONF,null);
+            String redissonUrl = properties.getOrDefault(REDISSON_LOCK_URL,null);
+            if(!Strings.isNullOrEmpty(redissonConfYmlBase64)){
+                String ymlConfig = new String(Base64.getDecoder().decode(redissonConfYmlBase64));
+                Config config = Config.fromYAML(ymlConfig);
+                redissonClient = Redisson.create(config);
+            }else if(!Strings.isNullOrEmpty(redissonUrl)){
+                Config config = new Config();
+                config.useClusterServers().addNodeAddress(redissonUrl);
+                redissonClient = Redisson.create(config);
+            }else{
+                String msg = String.format("[%s] or [%s] must be set.",REDISSON_LOCK_CONF,REDISSON_LOCK_URL);
+                throw new IllegalArgumentException(msg);
+            }
+            this.prefix = properties.getOrDefault(REDISSON_LOCK_KEY_PREFIX,null);
+            this.maxRetryTimes = Integer.parseInt(properties.getOrDefault(REDISSON_LOCK_RETRY_MAX_TIMES,"10"));
+        }catch (Exception e){
+            LOG.error("Unable to initialize redisson client.",e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        super.close();
+        for (RLock lock : lockCache.values()) {
+            if(lock.isLocked()){
+                lock.unlock();
+            }
+        }
+        redissonClient.shutdown();
+    }
+
+    @Override
+    public boolean acquire(String entityId, String ownerId) {
+        synchronized (lockCache){
+            try{
+                if(lockCache.get(ownerId)!=null){
+                    return false;
+                }
+                Tasks.foreach(entityId)
+                        .retry(maxRetryTimes - 1)
+                        .onlyRetryOn(BadRequestException.class)
+                        .throwFailureWhenFinished()
+                        .exponentialBackoff(acquireIntervalMs(), acquireIntervalMs(), acquireTimeoutMs(), 1)
+                        .run(id -> acquireOnce(id, ownerId));
+                return true;
+            }catch (Exception e){
+                LOG.error("An exception occurred during the locking process.",e);
+                return false;
+            }
+        }
+    }
+
+    private void acquireOnce(String entityId, String ownerId){
+        try{
+            String lockKey = prefix!=null?prefix+entityId:entityId;
+            RLock rlock = redissonClient.getLock(lockKey);
+            boolean success = rlock.tryLock(acquireTimeoutMs(),heartbeatTimeoutMs(), TimeUnit.MICROSECONDS);
+            if(!success){
+                throw new IllegalStateException("Unable to acquire lock " + entityId);
+            }
+            lockCache.putIfAbsent(ownerId,rlock);
+        }catch (InterruptedException e){
+            throw new BadRequestException(e,"connection refused.");
+        }
+    }
+
+    @Override
+    public boolean release(String entityId, String ownerId) {
+        synchronized (lockCache){
+            try{
+                RLock rlock = lockCache.get(ownerId);
+                if(rlock == null || !rlock.isLocked()){
+                    return true;
+                }
+                rlock.unlock();
+                lockCache.remove(ownerId);
+                return true;
+            }catch (Exception e){
+                LOG.error("An exception occurred during the unlocking process.",e);
+                return false;
+            }
+        }
+    }
+}
---
+++ core/src/main/java/org/apache/iceberg/lock/ServerSideHttpLockManager.java
@@ -0,0 +1,121 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  *   http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package org.apache.iceberg.lock;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Map;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.RequestAcceptEncoding;
+import org.apache.http.client.protocol.ResponseContentEncoding;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.iceberg.util.LockManagers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** User. */
+public class ServerSideHttpLockManager extends LockManagers.BaseLockManager{
+    private static final Logger LOG = LoggerFactory.getLogger(ServerSideHttpLockManager.class);
+    private String httpUrl = null;
+    private final static String OPERATOR = "operator";
+    private final static String LOCK = "lock";
+    private final static String UNLOCK = "unlock";
+    private final static String ENTITY_ID = "entityId";
+    private final static String OWNER_ID = "ownerId";
+    private final static int REQUEST_SUCCESS = 200;
+    public final static String REQUEST_URL = "lock.http.conf.request.url";
+    private HttpClient httpClient = null;
+    public ServerSideHttpLockManager(){}
+
+    public ServerSideHttpLockManager(String requestUrl){
+        init(requestUrl);
+    }
+
+    @Override
+    public boolean acquire(String entityId, String ownerId) {
+        return process(entityId,ownerId,LOCK);
+    }
+
+    @Override
+    public void initialize(Map<String, String> properties) {
+        super.initialize(properties);
+        String httpUrl = properties.getOrDefault(REQUEST_URL,null);
+        init(httpUrl);
+    }
+
+    private synchronized void init(String requestUrl){
+        if(requestUrl==null){
+            String msg = String.format("[%s] must be set.",REQUEST_URL);
+            throw new IllegalArgumentException(msg);
+        }
+        if(this.httpUrl==null){
+            this.httpUrl = requestUrl;
+        }
+        if(this.httpClient==null){
+            DefaultHttpClient defaultHttpClient = new DefaultHttpClient();
+            defaultHttpClient.addRequestInterceptor(new RequestAcceptEncoding());
+            defaultHttpClient.addResponseInterceptor(new ResponseContentEncoding());
+            this.httpClient = new DefaultHttpClient();
+        }
+    }
+
+    private String encode(String entity){
+        if(entity==null){
+            return null;
+        }
+        return new String(Base64.getUrlEncoder().encode(entity.getBytes(StandardCharsets.UTF_8)));
+    }
+
+    private boolean process(String entityId, String ownerId, String operator){
+        try{
+            HttpGet lockRequest = new HttpGet();
+            lockRequest.addHeader("Content-Type","application/x-www-form-urlencoded");
+            lockRequest.setURI(new URI(httpUrl));
+            HttpParams requestParams = new BasicHttpParams();
+            requestParams.setParameter(OWNER_ID,encode(ownerId));
+            requestParams.setParameter(ENTITY_ID,encode(entityId));
+            requestParams.setParameter(OPERATOR,operator);
+            lockRequest.setParams(requestParams);
+            HttpResponse response = httpClient.execute(lockRequest);
+            StatusLine stateLine = response.getStatusLine();
+            return REQUEST_SUCCESS == stateLine.getStatusCode();
+        }catch (URISyntaxException e){
+            throw new RuntimeException(e);
+        }catch (Exception e){
+            LOG.error("An exception occurred during the {} process.",operator,e);
+        }
+        return false;
+    }
+
+
+    @Override
+    public boolean release(String entityId, String ownerId) {
+        return process(entityId,ownerId,UNLOCK);
+    }
+}
